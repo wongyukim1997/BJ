3af6176fcc5a87fd26608aa8581a72f2c15ba698
wongyukim1997 <109955508+wongyukim1997@users.noreply.github.com>
2025-03-03 19:22:30 +0900

    [level 1] Title: 직사각형 별찍기, Time: 255.21 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        for(int i = 0; i < b; i++) {
+            for(int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
